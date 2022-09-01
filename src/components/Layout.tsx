852c853bde68cedb6f6ec3496953f63d4b34d583
gochelias <56095957+gochelias@users.noreply.github.com>
2022-08-31 21:13:02 -0500

    refactor: the layout was organized

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from 'react';
 import Head from 'next/head';
 
 import { Container } from 'components/Container';
-import { Nav } from 'components/Nav';
 import { Footer } from 'components/Footer';
 
 export const Layout = ({
@@ -17,13 +16,6 @@ export const Layout = ({
 			<title>{`${title} | Elias Goche`}</title>
 			<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 		</Head>
-		<div className="fixed z-0 flex h-screen w-full select-none flex-col justify-between px-16 pb-16">
-			<div>
-				<div className="h-16 bg-gradient-to-b from-black to-transparent" />
-				<Nav />
-			</div>
-			<div>© 2022</div>
-		</div>
 		<Container>
 			<div className="h-full w-full">{children}</div>
 			<Footer />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import { Nav } from 'components/Nav';
+
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<div className="fixed flex h-screen w-full select-none flex-col justify-between px-16 pb-16">
+				<div>
+					<div className="h-16 bg-gradient-to-b from-black to-transparent" />
+					<Nav />
+				</div>
+				<div>© 2022</div>
+			</div>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
