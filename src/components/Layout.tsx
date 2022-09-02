9a809973086ab070fbf312c931d6f7fef3825a32
gochelias <56095957+gochelias@users.noreply.github.com>
2022-09-02 17:22:35 -0500

    feat: a custom scrollbar has been added

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import Head from 'next/head';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import { Container } from 'components/Container';
 import { Footer } from 'components/Footer';
@@ -10,15 +11,29 @@ export const Layout = ({
 }: {
 	title: string;
 	children: ReactNode;
-}) => (
-	<>
-		<Head>
-			<title>{`${title} | Elias Goche`}</title>
-			<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-		</Head>
-		<Container>
-			<div className="h-full w-full">{children}</div>
-			<Footer />
-		</Container>
-	</>
-);
+}) => {
+	const scrollbar = () => <div className="rounded-full bg-[#333]" />;
+
+	return (
+		<>
+			<Head>
+				<title>{`${title} | Elias Goche`}</title>
+				<link
+					rel="shortcut icon"
+					href="favicon.ico"
+					type="image/x-icon"
+				/>
+			</Head>
+			<Scrollbars
+				renderThumbVertical={scrollbar}
+				universal
+				autoHeight
+				autoHeightMin="calc(100vh)">
+				<Container>
+					<div className="h-full w-full">{children}</div>
+					<Footer />
+				</Container>
+			</Scrollbars>
+		</>
+	);
+};
