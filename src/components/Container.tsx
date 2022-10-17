5fcc321a27b5004e1bed67eb8f126572fa1badc7
gochelias <56095957+gochelias@users.noreply.github.com>
2022-10-17 13:04:40 -0500

    feat: the padding of the container has been changed

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
 export const Container = ({ children }: { children: ReactNode }) => (
-	<div className="w-full px-4 md:px-6 lg:px-8 2xl:px-40">
+	<div className="w-full px-4 md:px-6 lg:px-8 2xl:px-16">
 		<div className="h-full w-full">{children}</div>
 	</div>
 );
