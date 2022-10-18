1babfee2eda0d525854caa2b8f15bcc9d7abbfb2
gochelias <56095957+gochelias@users.noreply.github.com>
2022-10-17 20:12:51 -0500

    feat: the footer has been redesigned and navigation has been added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
 import { ScrollTrigger, Tween } from 'react-gsap';
 import { nanoid } from 'nanoid';
 import CopyToClipboard from 'react-copy-to-clipboard';
@@ -12,11 +11,12 @@ import { ArrowRight } from 'components/ArrowRight';
 import { RecentProject } from 'components/RecentProject';
 
 const Home: NextPage = () => {
+	const apos = '\u2019';
 	const text =
 		'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat nesciunt fugiat, placeat tenetur ducimus aperiam suscipit illum sit explicabo.';
 	const textByWord = text.split(' ');
 
-	const [copied, setCopied] = useState(false);
+	// const [copied, setCopied] = useState(false);
 
 	return (
 		<Layout>
@@ -182,81 +182,158 @@ const Home: NextPage = () => {
 						/>
 					</div>
 				</section>
-				<footer className={styles.footer}>
-					<div className={styles.footer_main}>
-						<div className={styles.footer_content}>
-							<div className={styles.footer_copy_msg}>
-								{copied ? 'Copied' : 'Click to copy'}
-							</div>
-							<div className={styles.footer_email_container}>
-								<ScrollTrigger start="top bottom">
-									<Tween
-										from={{ opacity: 0 }}
-										duration={2.5}
-										ease="Expo.easeInOut">
-										<div>
-											<CopyToClipboard
-												text="hello@gochelias.com"
-												onCopy={() => setCopied(true)}>
-												<button
-													type="button"
-													className={
-														styles.footer_email
-													}>
-													<span>hello@</span>
-													<span>gochelias</span>
-													<span>.com</span>
-												</button>
-											</CopyToClipboard>
-										</div>
-									</Tween>
-								</ScrollTrigger>
-							</div>
-						</div>
+				<section className="relative flex h-screen w-full flex-col justify-center space-y-52 px-24">
+					<h6 className="select-none text-lg font-semibold uppercase">
+						Available for work
+					</h6>
+					<div className="text-9xl font-medium duration-75 hover:text-[#999999]">
+						<h2 className="inline select-none">
+							I like to work on cool stuff, let{apos}s work
+							together.{' '}
+						</h2>
+						<CopyToClipboard text="hello@gochelias.com">
+							<button
+								type="button"
+								className="text-[#FFEFCF] underline outline-none selection:bg-[#FFEFCF] selection:text-black active:bg-[#FFEFCF] active:text-black">
+								hello@gochelias.com
+							</button>
+						</CopyToClipboard>
 					</div>
-					<div className={styles.footer_info}>
-						<div className={styles.footer_copyright}>
-							© 2022 Elias Goche
-						</div>
-						<div className={styles.footer_social}>
-							<ScrollTrigger start="top bottom">
-								<Tween
-									from={{ opacity: 0 }}
-									duration={1.6}
-									stagger={0.2}
-									ease="Expo.easeInOut">
+					<footer className="flex w-full select-none justify-between">
+						<div>
+							<div className="group flex items-center space-x-3">
+								<Link href="/portfolio">
+									<a className="text-lg font-semibold uppercase">
+										Portfolio
+									</a>
+								</Link>
+								<div className="overflow-hidden">
+									<ArrowRight className="h-max w-4 -translate-x-full transition-transform duration-100 group-hover:translate-x-0" />
+								</div>
+							</div>
+							<ul className="mt-4 space-y-1 text-lg font-medium text-[#eaeaea]">
+								<li className="hover:underline">
 									<a
 										target="_blank"
 										href="https://github.com/gochelias"
 										title="@gochelias"
-										className={styles.footer_social_link}
+										className=""
 										rel="noreferrer">
 										GitHub
 									</a>
+								</li>
+								<li className="hover:underline">
+									<a
+										target="_blank"
+										href="https://codepen.io/gochelias"
+										title="@gochelias"
+										className=""
+										rel="noreferrer">
+										Codepen
+									</a>
+								</li>
+								<li className="hover:underline">
+									<a
+										target="_blank"
+										href="https://dribbble.com/gochelias"
+										title="@gochelias"
+										className=""
+										rel="noreferrer">
+										Dribbble
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div>
+							<div className="group flex items-center space-x-3">
+								<Link href="/about">
+									<a className="text-lg font-semibold uppercase">
+										About
+									</a>
+								</Link>
+								<div className="overflow-hidden">
+									<ArrowRight className="h-max w-4 -translate-x-full transition-transform duration-100 group-hover:translate-x-0" />
+								</div>
+							</div>
+							<ul className="mt-4 space-y-1 text-lg font-medium text-[#eaeaea]">
+								<li className="hover:underline">
+									<Link href="/me">
+										<a>Me</a>
+									</Link>
+								</li>
+								<li className="hover:underline">
+									<Link href="/feed">
+										<a>Feed</a>
+									</Link>
+								</li>
+								<li className="hover:underline">
+									<Link href="/stats">
+										<a>Stats</a>
+									</Link>
+								</li>
+							</ul>
+						</div>
+						<div>
+							<div className="group flex items-center space-x-3">
+								<Link href="/about">
+									<a className="text-lg font-semibold uppercase">
+										Contact
+									</a>
+								</Link>
+								<div className="overflow-hidden">
+									<ArrowRight className="h-max w-4 -translate-x-full transition-transform duration-100 group-hover:translate-x-0" />
+								</div>
+							</div>
+							<ul className="mt-4 space-y-1 text-lg font-medium text-[#eaeaea]">
+								<li className="hover:underline">
 									<a
 										target="_blank"
 										href="https://www.linkedin.com/in/gochelias"
 										title="@gochelias"
-										className={styles.footer_social_link}
 										rel="noreferrer">
 										LinkedIn
 									</a>
+								</li>
+								<li className="hover:underline">
 									<a
 										target="_blank"
 										href="https://twitter.com/gochelias"
 										title="@gochelias"
-										className={styles.footer_social_link}
 										rel="noreferrer">
 										Twitter
 									</a>
-								</Tween>
-							</ScrollTrigger>
+								</li>
+								<li className="hover:underline">
+									<a
+										target="_blank"
+										href="https://discord.com/users/636351475307773963"
+										title="gochelias#2076"
+										rel="noreferrer">
+										Discord
+									</a>
+								</li>
+							</ul>
 						</div>
-						<div className={styles.footer_name}>
-							Elias Goche Software Developer
+						<div>
+							<div className="group flex items-center space-x-3">
+								<Link href="/blog">
+									<a className="text-lg font-semibold uppercase">
+										Blog
+									</a>
+								</Link>
+								<div className="overflow-hidden">
+									<ArrowRight className="h-max w-4 -translate-x-full transition-transform duration-100 group-hover:translate-x-0" />
+								</div>
+							</div>
+							<ul className="mt-4 space-y-1 text-lg font-medium text-[#eaeaea]">
+								<li>undefined</li>
+								<li>undefined</li>
+								<li>undefined</li>
+							</ul>
 						</div>
-					</div>
-				</footer>
+					</footer>
+					<div className="absolute bottom-16 left-0">© 2022</div>
+				</section>
 			</main>
 		</Layout>
 	);
