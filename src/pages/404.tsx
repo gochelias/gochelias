ab959638110567318db612738240cfca51aa37ec
gochelias <56095957+gochelias@users.noreply.github.com>
2022-10-21 20:14:40 -0500

    feat(404): the paragraph has been changed and a link has been added to redirect to the home page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,13 +2,19 @@ import { RingsWaves } from 'components/RingsWaves';
 import Link from 'next/link';
 
 const Error404 = () => (
-	<main className="h-screen w-full select-none p-16">
-		<div className="relative z-[1] flex h-full w-full flex-col items-center justify-center space-y-6">
+	<main className="grid h-screen w-full select-none place-content-center p-16">
+		<div className="relative z-[1] flex w-max flex-col items-center justify-center">
 			<h2 className="text-[22rem] font-semibold leading-none">404</h2>
-			<div className="flex items-end space-x-1 text-xl font-medium">
-				<p>Seems you are lost, come back</p>
+			<div className="flex flex-col items-center space-y-4">
+				<p className="text-xl font-medium">
+					Page not found, seems you are lost.
+				</p>
 				<Link href="/">
-					<a className="underline">Home</a>
+					<a className="flex items-center text-lg font-semibold uppercase hover:text-[#FFEFCF]">
+						back
+						<hr className="mx-4 h-[1px] w-20 bg-white" />
+						Home
+					</a>
 				</Link>
 			</div>
 		</div>
