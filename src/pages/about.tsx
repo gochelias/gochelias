57fc89245ae7241ddd9d30e9ffd0327eb7843e59
gochelias <56095957+gochelias@users.noreply.github.com>
2022-10-21 20:23:07 -0500

    feat(about): a mockup of the skills section has been added

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -51,6 +51,142 @@ const AboutPage: NextPage = () => {
 					</div>
 				</div>
 			</header>
+			<section className="my-16 flex w-full flex-wrap gap-x-4">
+				<a
+					href="https://typescriptlang.org"
+					title="TypeScript: JavaScript With Syntax For Types"
+					className="text-xl font-semibold uppercase">
+					Typescript
+				</a>
+				-
+				<a
+					href="https://reactjs.org"
+					title="React – A JavaScript library for building user interfaces"
+					className="text-xl font-semibold uppercase">
+					React
+				</a>
+				-
+				<a
+					href="https://nextjs.org"
+					title="Next.js by Vercel - The React Framework"
+					className="text-xl font-semibold uppercase">
+					Next.js
+				</a>
+				-
+				<a
+					href="https://svelte.dev"
+					title="Svelte • Cybernetically enhanced web apps"
+					className="text-xl font-semibold uppercase">
+					Svelte
+				</a>
+				-
+				<a
+					href="https://tailwindcss.com"
+					title="Tailwind CSS - Rapidly build modern websites without ever leaving your HTML."
+					className="text-xl font-semibold uppercase">
+					Tailwindcss
+				</a>
+				-
+				<a
+					href="https://expressjs.com"
+					title="Express - Node.js web application framework"
+					className="text-xl font-semibold uppercase">
+					Express
+				</a>
+				-
+				<a
+					href="https:/hapi.dev"
+					title="hapi.dev - The simple, secure framework developers trust"
+					className="text-xl font-semibold uppercase">
+					Hapi
+				</a>
+				-
+				<a
+					href="https://nestjs.com"
+					title="NestJS - A progressive Node.js framework"
+					className="text-xl font-semibold uppercase">
+					NestJS
+				</a>
+				-
+				<a
+					href="https://prisma.io"
+					title="Prisma | Next-generation ORM for Node.js & TypeScript"
+					className="text-xl font-semibold uppercase">
+					Prisma
+				</a>
+				-
+				<a
+					href="jestjs.io"
+					title="Jest · 🃏 Delightful JavaScript Testing"
+					className="text-xl font-semibold uppercase">
+					Jest
+				</a>
+				-
+				<a
+					href="https://rust-lang.org"
+					title="Rust Programming Language"
+					className="text-xl font-semibold uppercase">
+					Rust
+				</a>
+				-
+				<a
+					href="https://go.dev"
+					title="The Go Programming Language"
+					className="text-xl font-semibold uppercase">
+					Go
+				</a>
+				<a
+					href="https://gin-gonic.com"
+					title="The fastest full-featured web framework for Go"
+					className="text-xl font-semibold uppercase">
+					Gin
+				</a>
+				-
+				<a
+					href="https://echo.labstack.com"
+					title="High performance, minimalist Go web framework"
+					className="text-xl font-semibold uppercase">
+					Echo
+				</a>
+				-
+				<a
+					href="https://python.org"
+					title="Python is a programming language that lets you work quickly and integrate systems more effectively"
+					className="text-xl font-semibold uppercase">
+					Python
+				</a>
+				-
+				<a
+					href="https://www.mongodb.com"
+					title="The Developer Data Platform"
+					className="text-xl font-semibold uppercase">
+					MongoDB
+				</a>
+				-
+				<a
+					href="https://www.postgresql.org"
+					title="The world's most advanced open source database"
+					className="text-xl font-semibold uppercase">
+					PostgreSQL
+				</a>
+				-
+				<a
+					href="https://docker.com"
+					title="Develop faster. Run anywhere."
+					className="text-xl font-semibold uppercase">
+					Docker
+				</a>
+				-
+				<a
+					href="https://git-scm.com"
+					title="A distributed version control system"
+					className="text-xl font-semibold uppercase">
+					Git.
+				</a>
+				<p className="text-xl font-medium text-[#999]">
+					And I keep learning more...
+				</p>
+			</section>
 			<section className="flex h-full w-full flex-col items-end">
 				<div className="flex h-screen w-4/5 flex-col justify-center">
 					<div className="flex h-96 w-full items-center justify-between">
