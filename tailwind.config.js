24a790c62cebce9dbfac139e8cf82dd7b4c4f100
gochelias <56095957+gochelias@users.noreply.github.com>
2023-01-07 19:23:54 -0600

    chore: customized gray colors have been added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ const LineClamp = require('@tailwindcss/line-clamp');
 module.exports = {
 	content: ['./src/**/*.{ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				gray6light: '#F2F2F7',
+				gray5light: '#E5E5EA',
+				gray4light: '#D1D1D6',
+				gray3light: '#C7C7CC',
+				gray2light: '#AEAEB2',
+				gray: '#8F8F93',
+				gray2dark: '#626266',
+				gray3dark: '#474749',
+				gray4dark: '#3B3B3D',
+				gray5dark: '#2C2C2D',
+				gray6dark: '#1C1C1E',
+			},
+		},
 		fontFamily: {
 			display: ['"Clash Display"'],
 			body: ['Telegraf'],
