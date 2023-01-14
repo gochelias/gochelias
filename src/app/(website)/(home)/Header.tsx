5e0b57f4e51e17c09d554dd8a406baa3e2f7d25b
gochelias <56095957+gochelias@users.noreply.github.com>
2023-01-14 12:34:32 -0600

    refactor: commented code has been removed

--- src/app/(website)/(home)/Header.tsx
+++ src/app/(website)/(home)/Header.tsx
@@ -1,137 +1,53 @@
+import Link from 'next/link';
+
 import { ArrowUpRight } from 'components/ArrowRight';
 import { Divider } from 'components/Divider';
-import Link from 'next/link';
 /* import styles from './Home.module.css'; */
 
-export const HeaderHome = () => {
-	const a = '';
-	return (
-		<header className="flex h-max w-full select-none flex-col pt-4">
-			<div id="top">
-				<Divider />
-				<div className="flex items-center justify-between py-4 pr-4 text-base uppercase leading-none">
-					<h6>Frontend Development</h6>
-					<h6>Backend Development</h6>
-					<h6>Apps Development</h6>
-					<Link
-						className="transition-colors hover:text-gray3light"
-						href="/contact">
-						Contact
-					</Link>
-				</div>
-				<Divider />
-				<div className="flex h-40 items-center justify-between">
-					<h1 className="text-6xl uppercase">
-						Elias <br /> Goche
-					</h1>
-					{/* <button
+export const HeaderHome = () => (
+	<header className="flex h-max w-full select-none flex-col pt-4">
+		<div id="top">
+			<Divider />
+			<div className="flex items-center justify-between py-4 pr-4 text-base uppercase leading-none">
+				<h6>Frontend Development</h6>
+				<h6>Backend Development</h6>
+				<h6>Apps Development</h6>
+				<Link
+					className="transition-colors hover:text-gray3light"
+					href="/contact">
+					Contact
+				</Link>
+			</div>
+			<Divider />
+			<div className="flex h-40 items-center justify-between">
+				<h1 className="text-6xl uppercase">
+					Elias <br /> Goche
+				</h1>
+				{/* <button
 						className="text mr-4 h-40 text-6xl uppercase"
 						type="button">
 						Me
 						<br />
 						nu
 					</button> */}
-				</div>
-				<Divider />
 			</div>
-			<div className="mt-8 ml-auto w-2/3">
-				<div className="w-max">
-					<h4 className="mb-6 text-4xl uppercase">
-						Software <br /> Developer
-					</h4>
-					<div className="flex items-end justify-between">
-						<h6 className="text-lg uppercase leading-tight">
-							Based in <br /> México
-						</h6>
-						<ArrowUpRight strokeWidth={3} className="m-1" />
-					</div>
-				</div>
-				<p className="mt-16 text-[8vw] uppercase leading-none">
-					Making <br /> the code an <br /> experience
-				</p>
-			</div>
-		</header>
-	);
-};
-
-/* <header className={styles.hero}>
-	<div
-		className={styles.hero_title}
-		data-scroll
-		data-scroll-speed="2">
-		<h2 className={styles.hero_name} data-scroll>
-			<Tween
-				from={{ y: 200, opacity: 0 }}
-				to={{ y: 0, opacity: 1 }}
-				duration={2}
-				delay={4}
-				ease="Expo.easeInOut">
-				<span className="block">Elias Goche</span>
-			</Tween>
-		</h2>
-		<h2 className={styles.hero_dev}>
-			<Tween
-				from={{ y: 200, opacity: 0 }}
-				duration={2}
-				delay={4.1}
-				ease="Expo.easeInOut">
-				<span className="block">Software Developer</span>
-			</Tween>
-		</h2>
-	</div>
-	<div className={styles.hero_center}>
-		<Tween
-			from={{ x: 100, opacity: 0 }}
-			to={{ x: 0, opacity: 1 }}
-			duration={1.8}
-			delay={4}
-			ease="Expo.easeInOut">
-			<div className={styles.hero_center_left}>
-				<div className={styles.hero_to_about}>
-					<ArrowRight className={styles.hero_arrow} />
+			<Divider />
+		</div>
+		<div className="mt-8 ml-auto w-2/3">
+			<div className="w-max">
+				<h4 className="mb-6 text-4xl uppercase">
+					Software <br /> Developer
+				</h4>
+				<div className="flex items-end justify-between">
+					<h6 className="text-lg uppercase leading-tight">
+						Based in <br /> México
+					</h6>
+					<ArrowUpRight strokeWidth={3} className="m-1" />
 				</div>
 			</div>
-		</Tween>
-		<Tween duration={2.4} delay={4} ease="Expo.easeInOut">
-			<div className={styles.hero_img_container}>
-				<Tween
-					to={{ y: '-101%' }}
-					duration={3}
-					delay={1}
-					ease="Expo.easeInOut">
-					<div className={styles.hero_img_cover} />
-				</Tween>
-				<Tween
-					from={{ opacity: 0, scale: 1.4 }}
-					to={{ opacity: 1, scale: 1.01 }}
-					duration={4}
-					delay={0.5}
-					ease="Expo.easeInOut">
-					<div className={styles.hero_img_layout}>
-						<Image
-							className={`${styles.hero_image}`}
-							fill
-							sizes="25%"
-							src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-							alt=""
-						/>
-					</div>
-				</Tween>
-			</div>
-		</Tween>
-		<Tween
-			from={{ x: -100, opacity: 0 }}
-			to={{ x: 0, opacity: 1 }}
-			duration={1.8}
-			delay={4}
-			ease="Expo.easeInOut">
-			<div className={styles.hero_center_right}>
-				<div className={styles.hero_available}>
-					<span className={styles.hero_available_text}>
-						Available for work
-					</span>
-				</div>
-			</div>
-		</Tween>
-	</div>
-</header> */
+			<p className="mt-16 text-[8vw] uppercase leading-none">
+				Making <br /> the code an <br /> experience
+			</p>
+		</div>
+	</header>
+);
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 
 import { FooterExternalLink } from 'components/FooterExternalLink';
 import { links } from 'config/links';
-import { Logo } from './Logo';
 import { ArrowUpRight } from './ArrowRight';
 
 export const Footer = () => {
@@ -66,97 +65,3 @@ export const Footer = () => {
 		</section>
 	);
 };
-
-/* 
-
-<section className="flex h-screen w-full flex-col justify-center space-y-64">
-			<h6 className="select-none text-lg font-medium uppercase">
-				Available for work
-			</h6>
-			<div className="group text-9xl font-medium">
-				<h2 className="inline select-none transition-colors duration-75 ease-in group-hover:text-gray2dark">
-					I like to work on cool stuff, let{apos}s work together.{' '}
-				</h2>
-				<Link
-					href="/contact"
-					className="text-gray underline outline-none transition-colors duration-75 ease-in selection:bg-white selection:text-black group-hover:text-white">
-					hello@gochelias.com
-				</Link>
-			</div>
-			<footer className="mr-40 flex w-auto select-none justify-between">
-				<div className="space-y-6">
-					<Link
-						href="/portfolio"
-						className="text-sm font-medium uppercase text-gray">
-						Portfolio
-					</Link>
-					<ul className="space-y-2 font-body text-lg">
-						<FooterExternalLink
-							title={links.github.username}
-							name={links.github.name}
-							url={links.github.url}
-						/>
-						<FooterExternalLink
-							title={links.codepen.username}
-							name={links.codepen.name}
-							url={links.codepen.url}
-						/>
-						<FooterExternalLink
-							title={links.dribbble.username}
-							name={links.dribbble.name}
-							url={links.dribbble.url}
-						/>
-					</ul>
-				</div>
-				<div className="space-y-6">
-					<Link
-						href="/about"
-						className="text-sm font-medium uppercase text-gray">
-						About
-					</Link>
-					<ul className="space-y-2 font-body text-lg">
-						<FooterLink name="Me" url="/me" />
-						<FooterLink name="Feed" url="/feed" />
-						<FooterLink name="stats" url="/stats" />
-					</ul>
-				</div>
-				<div className="space-y-6">
-					<Link
-						href="/contact"
-						className="text-sm font-medium uppercase text-gray">
-						Contact
-					</Link>
-					<ul className="space-y-2 font-body text-lg">
-						<FooterExternalLink
-							title={links.linkedin.username}
-							name={links.linkedin.name}
-							url={links.linkedin.url}
-						/>
-						<FooterExternalLink
-							title={links.discord.username}
-							name={links.discord.name}
-							url={links.discord.url}
-						/>
-						<FooterExternalLink
-							title={links.twitter.username}
-							name={links.twitter.name}
-							url={links.twitter.url}
-						/>
-					</ul>
-				</div>
-				<div className="space-y-6">
-					<Link
-						href="/blog"
-						className="text-sm font-medium uppercase text-gray">
-						Blog
-					</Link>
-					<ul className="space-y-2 font-body text-lg">
-						<li>undefined</li>
-						<li>undefined</li>
-						<li>undefined</li>
-					</ul>
-				</div>
-			</footer>
-		</section>
-
-*/
