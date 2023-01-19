77589b8857a7bb6877a1d57490364740415d6eb1
gochelias <56095957+gochelias@users.noreply.github.com>
2023-01-18 18:46:36 -0600

    feat(home): an animation has been added when the page has loaded

--- src/app/(website)/(home)/page.tsx
+++ src/app/(website)/(home)/page.tsx
@@ -1,22 +1,27 @@
 'use client';
 
-import styles from './Home.module.css';
+import { Tween } from 'react-gsap';
+import { Expo } from 'gsap';
+
 import { HeaderHome } from './Header';
 import { AboutSection } from './AboutSection';
 import { ProjectsSection } from './ProjectsSection';
+import styles from './Home.module.css';
 
 const HomePage = () => (
 	/* const containerRef = useRef(null); */
 	<>
 		{/* <LocomotiveScrollProvider
-					options={{ smooth: true, getSpeed: true }}> */}
-		<div>
-			<HeaderHome />
-			<main className={styles.main}>
-				<AboutSection />
-				<ProjectsSection />
-			</main>
-		</div>
+						options={{ smooth: true, getSpeed: true }}> */}
+		<Tween to={{ y: 0, opacity: 1 }} duration={0.8} ease={Expo.easeInOut}>
+			<div className="translate-y-4 opacity-0">
+				<HeaderHome />
+				<main className={styles.main}>
+					<AboutSection />
+					<ProjectsSection />
+				</main>
+			</div>
+		</Tween>
 		{/* </LocomotiveScrollProvider> */}
 	</>
 );
