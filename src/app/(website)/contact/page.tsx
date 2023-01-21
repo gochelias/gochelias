4844b3f1b929568b868bd60c13c1f4a4afe79105
gochelias <56095957+gochelias@users.noreply.github.com>
2023-01-20 18:01:30 -0600

    feat(contact): an animation has been added when the contact page has loaded

--- src/app/(website)/contact/page.tsx
+++ src/app/(website)/contact/page.tsx
@@ -2,6 +2,8 @@
 
 import { ChangeEvent, FormEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { Expo } from 'gsap';
+import { Tween } from 'react-gsap';
 
 import { ArrowUpRight } from 'components/ArrowRight';
 import { SocialLink } from 'components/SocialLink';
@@ -62,90 +64,101 @@ const ContactPage = () => {
 	};
 
 	return (
-		<header className={styles.container}>
-			<div className={styles.leftContainer}>
-				<div className={styles.leftContent}>
-					<p className={styles.textP}>
-						If you have a new project or want to say hello
-					</p>
-					<h2 className={styles.textH2}>
-						<span>Let{apos}s</span>
-						<span className={styles.indented}>get in</span>
-						<span>touch</span>
-					</h2>
-				</div>
-			</div>
-			<div className={styles.rightContainer}>
-				<form onSubmit={handleSubmit} className={styles.form}>
-					<div className={styles.formInputs}>
-						<div
-							className={`${styles.formInputContainer} ${styles.formInputName}`}>
-							<label className={styles.formLabel} htmlFor="name">
-								Name
-							</label>
-							<input
-								className={styles.formInput}
-								onChange={handleChange}
-								value={formData.name}
-								placeholder="Enter your name"
-								autoComplete="off"
-								type="text"
-								name="name"
-								id="name"
-								required
-							/>
-						</div>
-						<div
-							className={`${styles.formInputContainer} ${styles.formInputEmail}`}>
-							<label className={styles.formLabel} htmlFor="email">
-								Email
-							</label>
-							<input
-								className={styles.formInput}
-								onChange={handleChange}
-								value={formData.email}
-								placeholder="Enter your email"
-								type="email"
-								name="email"
-								id="email"
-								required
-							/>
-						</div>
-						<div className={styles.formTextareaContainer}>
-							<label
-								className={styles.formLabel}
-								htmlFor="message">
-								Message
-							</label>
-							<textarea
-								className={styles.formTextarea}
-								onChange={handleChange}
-								value={formData.message}
-								placeholder="Enter your message"
-								name="message"
-								id="message"
-								maxLength={280}
-								required
-							/>
+		<div className="overflow-hidden">
+			<Tween
+				to={{ y: 0, opacity: 1 }}
+				duration={0.8}
+				ease={Expo.easeInOut}>
+				<header className={styles.container}>
+					<div className={styles.leftContainer}>
+						<div className={styles.leftContent}>
+							<p className={styles.textP}>
+								If you have a new project or want to say hello
+							</p>
+							<h2 className={styles.textH2}>
+								<span>Let{apos}s</span>
+								<span className={styles.indented}>get in</span>
+								<span>touch</span>
+							</h2>
 						</div>
 					</div>
-					<button
-						className={styles.formButton}
-						type="submit"
-						title="Send">
-						<ArrowUpRight
-							className="h-full w-full rotate-45"
-							strokeWidth={2.5}
-						/>
-					</button>
-				</form>
-				<div className={styles.links}>
-					<SocialLink type={socialLinks.github} />
-					<SocialLink type={socialLinks.linkedin} />
-					<SocialLink type={socialLinks.discord} />
-				</div>
-			</div>
-		</header>
+					<div className={styles.rightContainer}>
+						<form onSubmit={handleSubmit} className={styles.form}>
+							<div className={styles.formInputs}>
+								<div
+									className={`${styles.formInputContainer} ${styles.formInputName}`}>
+									<label
+										className={styles.formLabel}
+										htmlFor="name">
+										Name
+									</label>
+									<input
+										className={styles.formInput}
+										onChange={handleChange}
+										value={formData.name}
+										placeholder="Enter your name"
+										autoComplete="off"
+										type="text"
+										name="name"
+										id="name"
+										required
+									/>
+								</div>
+								<div
+									className={`${styles.formInputContainer} ${styles.formInputEmail}`}>
+									<label
+										className={styles.formLabel}
+										htmlFor="email">
+										Email
+									</label>
+									<input
+										className={styles.formInput}
+										onChange={handleChange}
+										value={formData.email}
+										placeholder="Enter your email"
+										type="email"
+										name="email"
+										id="email"
+										required
+									/>
+								</div>
+								<div className={styles.formTextareaContainer}>
+									<label
+										className={styles.formLabel}
+										htmlFor="message">
+										Message
+									</label>
+									<textarea
+										className={styles.formTextarea}
+										onChange={handleChange}
+										value={formData.message}
+										placeholder="Enter your message"
+										name="message"
+										id="message"
+										maxLength={280}
+										required
+									/>
+								</div>
+							</div>
+							<button
+								className={styles.formButton}
+								type="submit"
+								title="Send">
+								<ArrowUpRight
+									className="h-full w-full rotate-45"
+									strokeWidth={2.5}
+								/>
+							</button>
+						</form>
+						<div className={styles.links}>
+							<SocialLink type={socialLinks.github} />
+							<SocialLink type={socialLinks.linkedin} />
+							<SocialLink type={socialLinks.discord} />
+						</div>
+					</div>
+				</header>
+			</Tween>
+		</div>
 	);
 };
 
