8d3e9365e7a2196177dc69bef3d9364f69bc7e85
gochelias <56095957+gochelias@users.noreply.github.com>
2023-01-26 12:11:32 -0600

    chore: vitest has been extended and linter rules for rtl have been added

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	root: './src',
-	plugins: [react()],
+	root: './',
+	plugins: [react(), tsconfigPaths()],
 	test: {
+		dir: 'src',
+		globals: true,
+		watch: false,
 		environment: 'jsdom',
+		setupFiles: ['vitest.setup.ts'],
+		reporters: process.env.CI ? 'basic' : 'verbose',
 	},
 });
