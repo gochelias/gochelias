a30a7628c83823c11904f6c6eace3384486174b1
gochelias <56095957+gochelias@users.noreply.github.com>
2023-06-14 14:06:32 -0600

    refactor: change the exported object of mdx components

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -5,9 +5,9 @@ import Quote from './Quote';
 import Repo from './Repo';
 
 export default {
-	Pkg: (props: any) => <Pkg {...props} />,
-	Quote: (props: any) => <Quote {...props} />,
-	Repo: (props: any) => <Repo {...props} />,
-	Code: (props: any) => <Code {...props} />,
-	Cmd: (props: any) => <Cmd {...props} />,
+	Pkg,
+	Quote,
+	Repo,
+	Code,
+	Cmd,
 };
