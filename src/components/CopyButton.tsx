f6818b16447f6288a6d51273653a562f841684eb
gochelias <56095957+gochelias@users.noreply.github.com>
2023-06-15 13:44:38 -0600

    feat: add animation to copy button when hovering and clicking

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,13 +1,27 @@
 'use client';
 
+import { useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Copy } from 'react-feather';
+import { Check, Copy } from 'react-feather';
 
 export default function CopyButton({ text }: { text: string }) {
+	const [isCopied, setIsCopied] = useState<boolean>(false);
+
+	const copy = (): void => {
+		setIsCopied(true);
+
+		setTimeout(() => {
+			setIsCopied(false);
+		}, 1000);
+	};
+
 	return (
-		<CopyToClipboard text={text}>
-			<button className="p-4 text-gray" type="button">
-				<Copy />
+		<CopyToClipboard text={text} onCopy={copy}>
+			<button
+				className="p-4 text-gray transition-colors hover:text-white"
+				title="Copy"
+				type="button">
+				{isCopied ? <Check className="text-white" /> : <Copy />}
 			</button>
 		</CopyToClipboard>
 	);
