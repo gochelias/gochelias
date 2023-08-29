251658bf59ddf77e69fe233c5b16c75c19895fd4
gochelias <56095957+gochelias@users.noreply.github.com>
2023-08-28 19:02:39 -0600

    refactor: remove unused code and dependencies

--- src/app/(website)/(home)/page.tsx
+++ src/app/(website)/(home)/page.tsx
@@ -10,21 +10,15 @@ import { ProjectsSection } from './ProjectsSection';
 import styles from './Home.module.css';
 
 const HomePage = () => (
-	/* const containerRef = useRef(null); */
-	<>
-		{/* <LocomotiveScrollProvider
-						options={{ smooth: true, getSpeed: true }}> */}
-		<Tween to={{ y: 0, opacity: 1 }} duration={0.8} ease={Expo.easeInOut}>
-			<div className="translate-y-4 opacity-0">
-				<HeaderHome />
-				<main className={styles.main}>
-					<AboutSection />
-					<ProjectsSection />
-				</main>
-				<Footer />
-			</div>
-		</Tween>
-		{/* </LocomotiveScrollProvider> */}
-	</>
+	<Tween to={{ y: 0, opacity: 1 }} duration={0.8} ease={Expo.easeInOut}>
+		<div className="translate-y-4 opacity-0">
+			<HeaderHome />
+			<main className={styles.main}>
+				<AboutSection />
+				<ProjectsSection />
+			</main>
+			<Footer />
+		</div>
+	</Tween>
 );
 export default HomePage;
